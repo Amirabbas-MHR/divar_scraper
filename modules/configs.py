c0e7602fab21077d7787e7ee715cc0cf1d5a579d
amirabbasMehrdad <aa.mehrdad82@gmail.com>
2024-02-08 21:39:24 +0330

    config files are loaded here from now and imported elsewhere

---
+++ modules/configs.py
@@ -0,0 +1,17 @@
+import json
+
+# loading valid categories list
+CATEGORY_FILE_PATH = 'config/categories.json'
+with open(CATEGORY_FILE_PATH, 'r') as category_file:
+    VALID_CATEGORIES = json.load(category_file)['categories']
+
+# loading valid enums list
+ENUMS_FILE_PATH = 'config/tehran.json'
+with open(ENUMS_FILE_PATH, 'r') as enums_file:
+    nears = json.load(enums_file)['nears']
+    VALID_ENUMS = [near['enum'] for near in nears]
+
+# loading the mapper of category name to it's uri, used to send the initial get request
+URI_SCHEME_FILE_PATH = 'config/uri_scheme.json'
+with open(URI_SCHEME_FILE_PATH, 'r') as uri_scheme_file:
+    URI_SCHEMA = json.load(uri_scheme_file)['urischema']['display']
